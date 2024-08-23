b3dc455855ef4b0ff5fd907779fd40fbefd3e152
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-23 02:35:57 -0300

    adjustment page example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AngularVlibras } from '../../projects/angular-vlibras/src/lib/angular-vlibras.component';
+import { AngularVlibras } from 'angular-vlibras';
+import { AngularTechsLogosModule } from 'angular-techs-logos';
+// import { AngularVlibras } from '../../projects/angular-vlibras/src/lib/angular-vlibras.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AngularVlibras],
+  imports: [AngularVlibras, AngularTechsLogosModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-vlibras';
+  title = 'vlibras';
 }
