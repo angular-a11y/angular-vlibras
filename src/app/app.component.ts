1bfdf7d1a7b9b5cb91a0491cdd6a214ff2daa927
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-23 01:29:02 -0300

    initial config use library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularVlibras } from '../../projects/angular-vlibras/src/lib/angular-vlibras.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularVlibras],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
