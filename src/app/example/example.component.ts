0bd89f7591dbb075fe5712b381ffcdd9ba0db426
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-26 17:31:28 -0300

    add nucleus box

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NucleusBox } from 'nucleus-angular';
 import { AngularVlibras } from '../../../projects/angular-vlibras/src/public-api';
 
 @Component({
   selector: 'example-vlibras',
   standalone: true,
-  imports: [AngularVlibras],
+  imports: [AngularVlibras, NucleusBox],
   templateUrl: './example.component.html',
   styleUrl: './example.component.scss',
 })
